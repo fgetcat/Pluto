ff4d2f5f5690e256f8c6ad13ef8a6dbeb01f8385
Llennpie <44985633+Llennpie@users.noreply.github.com>
2024-08-05 17:36:41 -0400

    Finish animation UI

--- src/saturn/ui/saturn_imgui_animations.cpp
+++ src/saturn/ui/saturn_imgui_animations.cpp
@@ -38,6 +38,7 @@ void OpenAnimationsMenu() {
     if (ImGui::BeginTabBar("###animation_tab_bar")) {
         if (ImGui::BeginTabItem("Vanilla")) {
             enable_custom_anim = false;
+            ImGui::SetNextItemWidth(200);
             if (ImGui::BeginCombo("###v_anim_combo", saturn_animations[selected_anim_index], ImGuiComboFlags_None)) {
                 ImGui::InputTextWithHint("###anim_search", "Search...", animSearchTerm, IM_ARRAYSIZE(animSearchTerm), ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_CharsUppercase);
                 ImGui::Separator();
@@ -59,25 +60,37 @@ void OpenAnimationsMenu() {
         }
         if (ImGui::BeginTabItem("PAnim")) {
             enable_custom_anim = true;
-            if (ImGui::BeginCombo("###p_anim_combo", pluto_animations_list[selected_panim_index].c_str(), ImGuiComboFlags_None)) {
-                ImGui::InputTextWithHint("###anim_search", "Search...", animSearchTerm, IM_ARRAYSIZE(animSearchTerm), ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_CharsUppercase);
-                ImGui::Separator();
-                for (int n = 0; n < pluto_animations_list.size(); n++) {
-                    const bool is_selected = (selected_panim_index == n);
-
-                    if (pluto_animations_list[n].find(animSearchTerm) == std::string::npos &&
-                        animSearchTerm != "")
-                        continue;
-
-                    if (ImGui::Selectable(pluto_animations_list[n].c_str(), is_selected)) {
-                        selected_panim_index = n;
-                        current_panim = LoadPAnim("dynos/anims/" + pluto_animations_list[n]);
-                    }
-
-                    if (is_selected) ImGui::SetItemDefaultFocus();
-                }
-                ImGui::EndCombo();
+            if (current_panim.Length == -1) {
+                current_panim = LoadPAnim("dynos/anims/" + pluto_animations_list[0]);
+                loop_anim = current_panim.Looping;
             }
+
+            ImGui::BeginChild("###p_anim_select", ImVec2(200, 125), ImGuiChildFlags_Border);
+            ImGui::SetNextItemWidth(200);
+            ImGui::InputTextWithHint("###anim_search", "Search...", animSearchTerm, IM_ARRAYSIZE(animSearchTerm), ImGuiInputTextFlags_AutoSelectAll);
+            ImGui::Separator();
+            for (int n = 0; n < pluto_animations_list.size(); n++) {
+                const bool is_selected = (selected_panim_index == n);
+
+                if (pluto_animations_list[n].find(animSearchTerm) == std::string::npos &&
+                    animSearchTerm != "")
+                    continue;
+
+                if (ImGui::Selectable(pluto_animations_list[n].c_str(), is_selected)) {
+                    selected_panim_index = n;
+                    current_panim = LoadPAnim("dynos/anims/" + pluto_animations_list[n]);
+                    loop_anim = current_panim.Looping;
+                }
+
+                if (is_selected) ImGui::SetItemDefaultFocus();
+            }
+            ImGui::EndChild();
+
+            ImGui::BeginChild("###p_metadata", ImVec2(200, 48), ImGuiChildFlags_Border);
+            ImGui::Text("%s", current_panim.Name.c_str());
+            ImGui::TextDisabled("@ %s", current_panim.Author.c_str());
+            ImGui::EndChild();
+
             ImGui::EndTabItem();
         }
         if (ImGui::IsItemClicked())
